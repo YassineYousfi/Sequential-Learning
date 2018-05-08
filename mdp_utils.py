741c605330a7b11ab8be6393d619da3cb135a15c
yassineyousfi <yassine.y10@gmail.com>
2018-05-08 10:46:18 +0100

    Update mdp_utils.py

--- mdp_utils.py
+++ mdp_utils.py
@@ -5,5 +5,5 @@ def samplefrom(distribution):
     return (np.random.choice(len(distribution), 1, p=distribution))[0]
 
 def playtransition(mdp, state, action):
-        nextstate = samplefrom(mdp.P[state][action])
-        return nextstate, mdp.R[state][action]
+        nextstate = samplefrom(mdp.P[action][state])
+        return nextstate, mdp.R[action][state]
